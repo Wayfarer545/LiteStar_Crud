5126085c9c2fa5ac96eae2ef56e4ff82c98c63cf
Sergei Firsov <sergey.firsov@aisa.ru>
2025-04-29 12:45:17 +0200

    add uow

---
+++ src/application/uow/utit_of_work.py
@@ -0,0 +1,39 @@
+from abc import abstractmethod, ABC
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from adapters.repositories.sql_user_repository import SqlUserRepository
+from domain.ports.user_repository import UserRepository
+
+
+class AbstractUnitOfWork(ABC):
+    users: UserRepository
+
+    @abstractmethod
+    async def commit(self) -> None:
+        pass
+
+    @abstractmethod
+    async def rollback(self) -> None:
+        pass
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            await self.rollback()
+        else:
+            await self.commit()
+
+
+class SqlUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.users = SqlUserRepository(session)
+
+    async def commit(self) -> None:
+        await self.session.commit()
+
+    async def rollback(self) -> None:
+        await self.session.rollback()
